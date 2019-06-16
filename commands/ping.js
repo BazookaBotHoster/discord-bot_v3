5474ca8eb9417c438498b489bf243eddedce5aaa
BazookaBotHoster <51861084+BazookaBotHoster@users.noreply.github.com>
2019-06-16 01:33:21 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,23 +1,19 @@
-// This commands requires 1 package
-const Discord = require('discord.js'); // We need this to form & send embeds.
+exports.run = (bot, msg, params) => {
+    Message.reply(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+         
 
-// Command Handler
-exports.run = async (client, message, args) => {
+};
 
-    const useruser = 'Command ran by: ' + message.author.username;
-    const userurl = message.author.avavtarURL;
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 4
+};
 
-    // Forming the embed
-    let embed = new Discord.RichEmbed() // Remember to use .MessageEmbed() if you use the master version
-        .setColor(0x43f033)
-        .setDescription(`Loading...`)
-        .setTimestamp()
-    message.channel.send(embed).then(message => {
-        embed.setColor(0x43f033)
-        embed.setDescription(`:ping_pong: Pong! **\`${client.pings[0]}ms\`**`)
-        embed.setFooter(useruser, userurl)
-        embed.setTimestamp()
-        message.edit(embed)
-    })
 
-} 
+exports.help = {
+  name: "ping",
+  description: "Get a simple Pong response",
+  usage: "ping"
+};
